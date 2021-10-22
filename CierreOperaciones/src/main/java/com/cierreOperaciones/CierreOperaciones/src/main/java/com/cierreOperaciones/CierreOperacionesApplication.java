8cfe8b8462e2fa7da88afa781cccb1e7cd4c3077
Luis Vargas <81274432+manuvargasg@users.noreply.github.com>
2021-10-22 01:37:47 -0500

    Add files via upload

---
+++ CierreOperaciones/src/main/java/com/cierreOperaciones/CierreOperaciones/src/main/java/com/cierreOperaciones/CierreOperacionesApplication.java
@@ -0,0 +1,13 @@
+package com.cierreOperaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CierreOperacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CierreOperacionesApplication.class, args);
+	}
+
+}
