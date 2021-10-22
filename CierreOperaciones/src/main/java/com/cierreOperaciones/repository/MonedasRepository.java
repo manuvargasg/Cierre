8a44aec8d68cc80c63868b73aab50e6a75bcc330
Luis Vargas <81274432+manuvargasg@users.noreply.github.com>
2021-10-22 01:38:31 -0500

    Delete CierreOperaciones directory

--- CierreOperaciones/src/main/java/com/cierreOperaciones/repository/MonedasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cierreOperaciones.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cierreOperaciones.model.Moneda;
-
-public interface MonedasRepository extends JpaRepository<Moneda, Long> {
-
-}
